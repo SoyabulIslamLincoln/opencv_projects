81c3491075a7beac5e10af3cb3a485daeebd1230
Soyabul Islam Lincoln <55865931+SoyabulIslamLincoln@users.noreply.github.com>
2020-12-11 00:31:15 +0600

    Add files via upload

--- web_app_for_face_detection/app.py
+++ web_app_for_face_detection/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, Response
-from capture import capture_video
+from capture import VideoCamera
+import time
 
 app = Flask(__name__)
 
@@ -9,12 +10,15 @@ def index():
 
 def gen(camera):
     while True:
-        frame = camera.get_video()
-        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+        time.sleep(0.2)
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(capture_video()),
+    return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
